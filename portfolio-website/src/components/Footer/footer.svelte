7f7124dd7318784cd00ed29195e60873929d5c39
sakib3046 <sakib3046@gmail.com>
2024-06-05 01:04:46 +0600

    Footer

---
+++ portfolio-website/src/components/Footer/footer.svelte
@@ -0,0 +1,37 @@
+<div class="w-full px-8 footer">
+  <div
+    class="z-50 top-0 left-0 my-4 flex flex-row justify-between w-full bg-gradient-to-r from-900 via-700 to-900 px-6 py-3 shadow-xl shadow-950/10 rounded-lg"
+  >
+    <div class="flex flex-col justify-start md:flex-row">
+      <div class=" flex flex-col p-4">
+        <h1 class="flex flex-col">
+          <span class="footerText text-4xl">Thank You</span><span
+            class="footerText text-2xl">So Much from</span
+          ><span class="footerText text-4xl md:text-6xl">MD SAKIB HASAN</span>
+        </h1>
+      </div>
+
+      <div class=" flex flex-col px-4 justify-center">
+        <a href="" class="footerText">About Me</a>
+        <a href="" class="footerText">Projects</a>
+        <a href="" class="footerText">Blog</a>
+        <a href="" class="footerText">Terms & Condition</a>
+      </div>
+      <div class="flex flex-col p-4">
+        <div class="flex flex-row">
+          <input type="text" class="rounded-xl bg-200 focus:bg-50" />
+          <button
+            class="bg-50 font-bold rounded-lg mx-2 px-2 hover:scale-105 transition duration-300 ease-in-out"
+            >Get In Touch</button
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .footerText {
+    @apply text-50 font-semibold;
+  }
+</style>
