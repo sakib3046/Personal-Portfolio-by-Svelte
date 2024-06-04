f38b46d82450c9e73b34b89eac22d9811332d56d
sakib3046 <sakib3046@gmail.com>
2024-06-05 01:11:41 +0600

    Directory change

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import imageUrlBuilder from '@sanity/image-url';
+  import{sanityClient} from '../lib/sanityClient';
+  import Header from "../components/header/header.svelte";
+  import About from '../components/about/about.svelte';
+  import Projects from '../components/projects/projects.svelte';
+  export let data;
+
+  const builder = imageUrlBuilder(sanityClient);
+
+  let headerimageUrl = builder.image(data.data[0].portrait).url();
+  console.log(data.data[0])
+
+</script>
+<div class="px-6 md:px-10">
+  <Header name={data.data[0].name} headerImage={headerimageUrl} />
+  <About/>
+  <Projects/>
+</div>
+
