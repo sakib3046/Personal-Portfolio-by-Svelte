d5238813acd21f19769fe2c4371bc80d3763e67a
sakib3046 <sakib3046@gmail.com>
2024-06-23 17:13:29 +0600

    portable text config

---
+++ src/components/ListItem.svelte
@@ -0,0 +1,7 @@
+<script >
+    export let portableText;
+    console.log(portableText)
+</script>
+<li class="bg-500 text-5xl ">
+    <slot/>
+</li>
--- src/routes/[blogs]/+page.svelte
+++ src/routes/[blogs]/+page.svelte
@@ -1,7 +1,45 @@
 <script>
     import {PortableText} from '@portabletext/svelte'
+    import ListItem from '../../components/ListItem.svelte'
     export let data;
     console.log(data.data[0])
 </script>
 
-<PortableText value={data.data[0].content}/>
+
+
+<PortableText
+        value={data.data[0].content}
+        components={{
+    // types: {
+    //   // block-level components
+    //   callout: Callout,
+    //   // inline-level components
+    //   userInfo: UserInfo
+    // },
+    // marks: {
+    //   absUrl: AbsoluteURL,
+    //   // Overwrite default mark renderers
+    //   strong: CustomStrong
+    // },
+    // block: {
+    //   normal: CustomParagraph,
+    //   blockquote: Quote,
+    //   // Re-using the same component across multiple styles
+    //   h1: CustomHeading,
+    //   h2: CustomHeading,
+    //   h3: CustomHeading,
+    //   // Custom user-defined style
+    //   textCenter: CentralizedText
+    // },
+    // list: {
+    //   // Swap only the list parts you need
+    //   bullet: UnorderedListWrapper,
+    //   // Custom user-defined list type
+    //   checklist: ChecklistWrapper
+    // },
+    list: {
+      bullet: ListItem,
+      // checklist: ChecklistItem
+    }
+  }}
+/>
